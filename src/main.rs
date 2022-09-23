48b2238c596f88778bfc811f072230740831ed4b
Marcus Dashe <marcusdashe.developer@gmail.com>
2022-09-23 09:45:44 +0100

    Utilize std error and closures

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use std::io::prelude::*;
 use greprs::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     let mut stderr = std::io::stderr();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         writeln!(
             &mut stderr, 
             "Problem parsing arguments: {}", 
